41b77aa0c7c167e96bf5abf39d77b89ec7c98179
lynndixon <lynn@redhat.com>
2025-09-19 17:50:18 -0400

    CH3-L4

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 from prompts import system_prompt
 from call_function import available_functions
+from call_function import call_function
 
 
 def main():
@@ -54,8 +55,14 @@ def generate_content(client, messages, verbose):
         return response.text
 
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-
+        function_call_result = call_function(function_call_part, verbose)
+        if not function_call_result.parts[0].function_response.response:
+            raise ValueError("Function call failed or returned no response")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+            print(f"{function_call_result.parts[0].function_response.response}")
+    
 
 if __name__ == "__main__":
     main()
