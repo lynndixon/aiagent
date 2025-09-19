8f7810c5d67069507fe19895bea429e4c1ad5a0e
lynndixon <lynn@redhat.com>
2025-09-19 10:39:06 -0400

    CH2-L5

---
+++ functions/run_python_file.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not abs_file_path.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:      
+        result = subprocess.run(
+            ["python", abs_file_path] + args,
+            capture_output=True,
+            text=True,
+            cwd=abs_working_dir,
+            timeout=30
+        )
+        std_out = result.stdout
+        std_error = result.stderr
+        if result.returncode != 0:
+            return f'Process exited with code {result.returncode}" : {result.stderr}'
+        if not std_out:
+            return f'No output produced. Stderr: {std_error}'
+        return f'STDOUT: {std_out}\nSTDERR: {std_error}'
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
